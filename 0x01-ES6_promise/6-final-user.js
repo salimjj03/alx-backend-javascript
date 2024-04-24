976020867a9bcc330b88df48abf19e75bdc7edac
salim aliyu musawa <salemaliyu1992@gmail.com>
2024-04-24 14:45:21 +0000

    sattiled promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.forEach((result) => ({
+    .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.value,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
     })));
 }
