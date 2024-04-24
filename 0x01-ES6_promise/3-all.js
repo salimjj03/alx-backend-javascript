08000680129d4395828936eca2024f3792676356
salim aliyu musawa <salemaliyu1992@gmail.com>
2024-04-24 11:42:06 +0000

    all promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([createUser(), uploadPhoto()])
+    .then((user) => {
+      console.log(`${user[1].body} ${user[0].firstName} ${user[0].lastName}`);
+    })
+    .catch((error) => {
+      console.error('Signup system offline');
+    });
+}
