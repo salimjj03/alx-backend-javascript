00ebc9557a97d1abe9b57e5cf404d77cffb4f7cd
salim aliyu musawa <salemaliyu1992@gmail.com>
2024-04-25 21:40:13 +0000

    throw error

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,43 +1,43 @@
 export default class HolbertonCourse {
-	constructor(name, length, students) {
-		this._name = name;
-		this._length = length;
-		this._students = students;
-	}
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-	set name(nm) {
-		if (typeof nm === "string") {
-			this._name = nm;
-		} else {
-			throw new TypeError("Name must be a string")
-		}
-	}
+  set name(nm) {
+    if (typeof nm === 'string') {
+      this._name = nm;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
 
-	set length(len) {
-		if (typeof len === "number") {
-                	this._length = len;
-		} else {
-			throw new TypeError("Length must be a number")
-		}
-        }
+  set length(len) {
+    if (typeof len === 'number') {
+      this._length = len;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
 
-	set students(std) {
-		if (Array.isArray(std)) {
-            		this._students = std;
-        	} else {
-            		throw new TypeError("Students must be an array");
-		}
-        }
+  set students(std) {
+    if (Array.isArray(std) && std.every((element) => typeof element === 'string')) {
+      this._students = std;
+    } else {
+      throw new TypeError('Students must be an array');
+    }
+  }
 
-	get name() {
-		return this._name;
-	}
+  get name() {
+    return this._name;
+  }
 
-	get length() {
-                return this._length;
-        }
+  get length() {
+    return this._length;
+  }
 
-	get students() {
-                return this._students;
-        }
+  get students() {
+    return this._students;
+  }
 }
