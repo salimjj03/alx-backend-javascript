c2c75bc07a1f20b25f60ae82441b0acc607da852
salim aliyu musawa <salemaliyu1992@gmail.com>
2024-04-26 11:27:31 +0000

    car class

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,41 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set brand(newvalue) {
+    this._brand = newvalue;
+  }
+
+  set motor(newvalue) {
+    this._motor = newvalue;
+  }
+
+  set color(newvalue) {
+    this._color = newvalue;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
