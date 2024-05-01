65ada98c30e21997e12c5986d5d117a7666a8a1f
salim aliyu musawa <salemaliyu1992@gmail.com>
2024-05-01 14:13:14 +0000

    type script

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,11 @@
+interface Student {
+	firstName: string,
+	lastName: string,
+	age: number,
+	location: string
+	}
+
+	const std1: Student = {'salim', 'aliyu', 31, 'katsina'}
+	const std2: Student = {'Husain', 'Ahmad', 35, 'Babura'}
+
+	const studentsList = [std1, std2]
