044da1192bb07c3f14797145e499d3e4fcbd3d51
salimjj03 <salemaliyu1992@gmail.com>
2024-06-18 12:12:32 +0100

    node js fs

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,33 @@
-module.exports = countStudents(database){
-file = require('fn')
-	file.readFile(database)
+const fs = require('fs');
 
+function countStudents(file) {
+  let data;
+  try {
+    data = fs.readFileSync(file);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  data = data.toString().split('\n');
+  let std = data.filter((item) => item);
+  std = std.map((item) => item.split(','));
+  const stdLen = std.length ? std.length - 1 : 0;
+  console.log(`Number of students: ${stdLen}`);
+
+  const fields = {};
+  std.forEach((std) => {
+    const field = std[3];
+    const firstName = std[0];
+
+    if (fields[field]) {
+      fields[field].push(firstName);
+    } else {
+      fields[field] = [firstName];
+    }
+  });
+
+  for (const [field, names] of Object.entries(fields)) {
+    console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+  }
 }
-console.log(process.argv[2])
+
+module.exports = countStudents;
