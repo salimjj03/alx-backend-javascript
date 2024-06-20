36032812c888900b77c7b529ea0a5805bf616cac
salimjj03 <salemaliyu1992@gmail.com>
2024-06-20 11:25:30 +0100

    test node

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,22 +5,23 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('should return sum of integers', () => {
+    assert.strictEqual(calculateNumber(2, -2), 0);
+    assert.strictEqual(calculateNumber(3, -5), -2);
     assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, -1), 0);
-    assert.strictEqual(calculateNumber(1, -3), -2);
+    assert.strictEqual(calculateNumber(10, 3), 13);
   });
 
   it('should round floats', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(2, 5.7), 8);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     assert.strictEqual(calculateNumber(0.1, 0), 0);
-    assert.strictEqual(calculateNumber(1.4, -4.5), -3);
+    assert.strictEqual(calculateNumber(2.4, -5.5), -3);
   });
 
   it('should return rounded sum if only one is provided', () => {
-    assert.strictEqual(calculateNumber(2), 2);
-    assert.strictEqual(calculateNumber(2.7), 3);
+    assert.strictEqual(calculateNumber(4.7), 5);
+    assert.strictEqual(calculateNumber(3), 3);
+    assert.strictEqual(calculateNumber(6.7), 7);
   });
 
   it('should cast non-numbers into numbers', () => {
